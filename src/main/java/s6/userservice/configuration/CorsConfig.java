20340dbeb10dfdb6c34945a75b85f8e5910342fb
Pieleanu,Andrei A <a.pieleanu@student.fontys.nl>
2024-11-26 15:55:53 +0100

    changed cors

--- src/main/java/s6/userservice/configuration/CorsConfig.java
+++ src/main/java/s6/userservice/configuration/CorsConfig.java
@@ -2,8 +2,6 @@ package s6.userservice.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
-import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import s6.userservice.Constants;
@@ -19,8 +17,7 @@ public class CorsConfig {
                 registry.addMapping("/**")
                         .allowedOrigins(Constants.KUBERNETES_FRONTEND_URL,
                                 Constants.LOCALHOST_FRONTEND_URL,
-                                Constants.FRONTEND_DOCKER_CONTAINER,
-                                CorsConfiguration.ALL) // Frontend or other allowed origins
+                                Constants.FRONTEND_DOCKER_CONTAINER) // Frontend or other allowed origins
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("Authorization", "Content-Type")
                         .allowCredentials(true);
